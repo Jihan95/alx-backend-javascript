9456f42cacff7c0e7f3ea38cb81a7321239dbfd4
Jihan <jihanahmed41@gmail.com>
2024-08-13 16:00:21 +0000

    task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
 export default function groceriesList() {
-  const groceriesList = new Map();
-  groceriesList.set('Apples', '10');
-  groceriesList.set('Tomatoes', '10');
-  groceriesList.set('Pasta', '1');
-  groceriesList.set('Rice', '1');
-  groceriesList.set('Banana', '5');
-  return groceriesList;
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
 }
