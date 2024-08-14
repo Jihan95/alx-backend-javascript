b7e1de43c4b52ac8ae388b71ca0f679edef69bd4
Jihan Ahmed <jihanahmed41@gmail.com>
2024-08-14 23:30:22 +0300

    new tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,46 @@
 interface Teacher {
-readonly firstName: string,
-readonly lastName: string,
-fullTimeEmployee: boolean,
-location: string,
-yearsOfExperience?: number,
-[key: string]: any 
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    location: string,
+    yearsOfExperience?: number,
+    [key: string]: any 
 }
 
 interface Directors extends Teacher {
-numberOfReports: number
+    numberOfReports: number
 }
+
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return `${this.firstName}`;
+    }
+}
