10a65baf358f9be3c09a2009c2001e9798d5eb59
jihan <jihan>
2024-10-01 12:41:55 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,23 +3,28 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8').trim();
-    const lines = data.split('\n').filter(line => line);
+    const lines = data.split('\n').filter((line) => line);
     const students = lines.slice(1);
+
     console.log(`Number of students: ${students.length}`);
     const fields = {};
-        
+
     students.forEach((student) => {
-    const [firstname, lastname, age, field] = student.split(',');
-    if (!fields[field]) {
-      fields[field] = [];
-    }
-    fields[field].push(firstname);
+      const [firstname, lastname, age, field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
     });
-        
+
     for (const field in fields) {
-      const studentsInField = fields[field];
-      console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
-  }
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const studentsInField = fields[field];
+        console.log(
+          `Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`,
+        );
+      }
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
