9fc050acbaf2b9fdc7570ebae05bbef9b5f60b7f
Jihan <jihanahmed41@gmail.com>
2024-11-03 07:00:55 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,9 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({'status': 200, 'body': 'Success'});
+      resolve({status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
-    reject(new Error('The fake API is not working currently'));
   });
 }
