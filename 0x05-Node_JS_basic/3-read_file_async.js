06c38d84e824c428433742614bdbcfb6d7bcf471
Jihan <jihanahmed41@gmail.com>
2024-11-07 11:54:18 +0000

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filePath)) {
+      return reject(new Error('Cannot load the database'));
+    }
+
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        return reject(new Error('Cannot load the database'));
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1); // Skip header line
+      console.log(`Number of students: ${students.length}`);
+
+      const fieldsCount = new Map();
+      students.forEach((student) => {
+        const info = student.split(',');
+        const firstName = info[0];
+        const field = info[3];
+
+        if (!fieldsCount.has(field)) {
+          fieldsCount.set(field, []);
+        }
+        fieldsCount.get(field).push(firstName);
+      });
+
+      fieldsCount.forEach((students, field) => {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
