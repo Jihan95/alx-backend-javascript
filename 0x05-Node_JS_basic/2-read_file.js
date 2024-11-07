ce9ac0014452fe4998f5ba30755373168da10407
Jihan <jihanahmed41@gmail.com>
2024-11-07 11:26:07 +0000

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,18 +1,17 @@
-const fs = require("fs")
-
+const fs = require('fs');
 
 function countStudents(filePath) {
   if (fs.existsSync(filePath)) {
-    let data = fs.readFileSync(filePath, "utf-8");
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1);
     console.log(`Number of students: ${students.length}`);
     const fieldsCount = new Map();
     students.forEach((student) => {
-      info = student.split(',');
-      firstName = info[0];
-      field = info[3];
-      if(!fieldsCount.has(field)) {
+      const info = student.split(',');
+      const firstName = info[0];
+      const field = info[3];
+      if (!fieldsCount.has(field)) {
         fieldsCount.set(field, []);
       }
       fieldsCount.get(field).push(firstName);
