7cdd472cd4b8a52663c81a0bcc84f1c2a209b1ff
Jihan <jihanahmed41@gmail.com>
2024-11-10 14:23:25 +0000

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,8 +1,21 @@
 const express = require('express');
+
 const app = express();
+
+const countStudents = require('./3-read_file_async');
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents('database.csv')
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.send(`This is the list of our students\n${error.message}`);
+    });
 });
 
 app.listen(1245, () => {
